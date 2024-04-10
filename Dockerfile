8fc4a399090290fbbef3d6afc8c266a9d60378f9
Julien Bernhart <julienbernhart@gmail.com>
2024-04-10 09:38:58 +0200

    initial

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM golang:1.22 as builder 
+
+# Set destination for COPY
+WORKDIR /app
+
+# Download Go modules
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY *.go ./
+
+# Build
+RUN CGO_ENABLED=0 GOOS=linux go build -o /bike-motivation
+
+FROM scratch
+
+COPY --from=builder /usr/share/zoneinfo /usr/share/zoneinfo
+COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+COPY --from=builder /etc/passwd /etc/passwd
+COPY --from=builder /etc/group /etc/group
+
+COPY --from=builder /bike-motivation /bike-motivation
+
+# Run
+CMD ["/bike-motivation"]
---
+++ motivation.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"cloud.google.com/go/vertexai/genai"
+	"github.com/bwmarrin/discordgo"
+)
+
+var model = "gemini-1.0-pro-001"
+
+var projectID string
+var region string
+var botToken string
+var channelID string
+
+func generate(projectId string, region string, modelName string) string {
+	ctx := context.Background()
+
+	client, err := genai.NewClient(ctx, projectId, region)
+	if err != nil {
+		log.Fatalf("error creating genai client: %v", err)
+	}
+	gemini := client.GenerativeModel(modelName)
+
+	prompt := genai.Text("Write a new poem to encourage someone to go outside and cycle, instead of staying inside and code on the computer.")
+	resp, err := gemini.GenerateContent(ctx, prompt)
+	if err != nil {
+		log.Fatalf("error generating content: %v", err)
+	}
+
+	return getFirstPart(resp)
+}
+
+func sendMessage(botToken string, channelID string, message string) {
+	discord, err := discordgo.New("Bot " + botToken)
+	if err != nil {
+		log.Fatalf("error sending message: %v", err)
+	}
+
+	discord.ChannelMessageSend(channelID, message)
+}
+
+func getFirstPart(resp *genai.GenerateContentResponse) string {
+	s := ""
+	buf := bytes.NewBufferString(s)
+	fmt.Fprint(buf, resp.Candidates[0].Content.Parts[0])
+
+	return buf.String()
+}
+
+func main() {
+	botToken = os.Getenv("DISCORD_BOT_TOKEN")
+	channelID = os.Getenv("DISCORD_CHANNEL_ID")
+	projectID = os.Getenv("RUN_PROJECT_ID")
+	region = os.Getenv("RUN_REGION")
+
+	if botToken == "" || channelID == "" || projectID == "" || region == "" {
+		log.Fatalf("missing environment variables")
+	}
+
+	sendMessage(botToken, channelID, generate(projectID, region, model))
+}
